8b96bf2b495c3088ade8fcddd7cd3ec013a8250b
faust <80465380+gaoubak@users.noreply.github.com>
2023-04-07 15:38:21 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,44 @@
 <?php
-
-if ($_SERVER['POST']) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $output = shell_exec("sudo ssh $username@$password");
-    echo "User $username logged in";
-
-    shell_exec("cd /home/$username; sudo touch index.php; echo '<?php phpinfo(); ?>' > index.php");
-    echo "User $username index.php created";
-
-    header("Location: /home/$username");
-    exit;
+function authenticate_user($connect, $username, $password) {
+    $stmt = $connect->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        if (password_verify($password, $user['password'])) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
+        $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+        if (authenticate_user($connect, $username, $password)) {
+            // User authenticated, set session variable and redirect to protected page
+            $_SESSION['username'] = $username;
+            header('Location: protected.php');
+            exit();
+        } else {
+            // Invalid username or password, display error message
+            $error_message = "Invalid username or password";
+        }
+    } else {
+        // Invalid CSRF token, display error message
+        $error_message = "Invalid CSRF token";
+    }
+}
+
+// Generate new CSRF token
+$_SESSION['token'] = bin2hex(random_bytes(32));
+
 ?>
 
 <html>
@@ -29,6 +55,8 @@ if ($_SERVER['POST']) {
             <input type="password" name="password" id="password" />
 
             <input type="submit" value="Se connecter" />
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
+
         </form>
     </body>
 </html>
