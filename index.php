160fe4dd1c24dfa3cc03a0171dd042193ae05b1f
faust <80465380+gaoubak@users.noreply.github.com>
2023-04-07 15:36:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
+session_start();
+$_SESSION['token'] = bin2hex(random_bytes(32));
 
-
+try {
+    $connect = new PDO('mysql:host=localhost:3306;dbname=potato_db', 'Kader', '');
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['username']) && isset($_POST['password'])) {
-        user_create($username, $password);
+        $stmt = $connect->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
     }        
 }
-
-
 function user_create($username, $password) {
-    $user = shell_exec("sudo useradd -m -p $password $username");
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $user = shell_exec("sudo useradd -m -p $hashed_password $username");
     echo "User $username created with password $password";
 
     $output = shell_exec("sudo usermod -aG sudo $username");
@@ -23,6 +30,15 @@ function user_create($username, $password) {
     $output = shell_exec("sudo chmod 755 /home/$username");
     echo "User $username home directory permissions changed";
 }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
+        $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+        user_create($username, $password);
+    }        
+}
+
+
 
 ?>
 
@@ -41,6 +57,7 @@ function user_create($username, $password) {
             <input type="password" name="password" id="password" />
 
             <input type="submit" value="S'inscrire" />
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
         </form>
         <a href="login.php">Se connecter</a>
     </body>
